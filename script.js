aab5172605c182fb23ec5a3abb9a70d2bdfe5625
Martina Leskovic <martina.leskovic@outlook.com>
2023-05-17 00:29:45 +0200

    Fix formatting

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 function getTime() {
-    let time = new Date();
-    let sec = time.getSeconds() / 60;
-    let min = (sec + time.getMinutes()) / 60;
-    let hr = (min + time.getHours()) / 12;
-    let secHandle = document.querySelector("#seconds-handle").style.transform = `rotate(${sec*360}deg)`;
-    let minHandle = document.querySelector("#minutes-handle").style.transform = `rotate(${min*360}deg)`;
-    let hrHandle = document.querySelector("#hours-handle").style.transform = `rotate(${hr*360}deg)`;
-    setTimeout(getTime, 1000);
+  let time = new Date();
+  let sec = time.getSeconds() / 60;
+  let min = (sec + time.getMinutes()) / 60;
+  let hr = (min + time.getHours()) / 12;
+  let secHandle = (document.querySelector(
+    "#seconds-handle"
+  ).style.transform = `rotate(${sec * 360}deg)`);
+  let minHandle = (document.querySelector(
+    "#minutes-handle"
+  ).style.transform = `rotate(${min * 360}deg)`);
+  let hrHandle = (document.querySelector(
+    "#hours-handle"
+  ).style.transform = `rotate(${hr * 360}deg)`);
+  setTimeout(getTime, 1000);
 }
 
 function getDate() {
-    let now = new Date();
-    let day = now.getUTCDate();
-    let month = now.getUTCMonth() + 1;
-    let year = now.getUTCFullYear();
-    let currentDate = document.querySelector("#current-date").innerText = `${day}.${month}.${year}.`;
+  let now = new Date();
+  let day = now.getUTCDate();
+  let month = now.getUTCMonth() + 1;
+  let year = now.getUTCFullYear();
+  let currentDate = (document.querySelector(
+    "#current-date"
+  ).innerText = `${day}.${month}.${year}.`);
 }
 
 getDate();
